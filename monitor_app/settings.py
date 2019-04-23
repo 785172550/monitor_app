599d08ac93ab8f8ff8a8191f489cd6c6e0e4dd64
kenneth <785172550@qq.com>
2019-04-23 20:13:53 +0800

    Default Changelist

--- monitor_app/settings.py
+++ monitor_app/settings.py
@@ -187,12 +187,20 @@ SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '572142719909-5kiam00ilsqpd6c58u8sq0rf0t9poi03.a
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = 'W2d7Xt6ylyGuEzpGaSU5VDAw'
 
 # smtp email server, you can change it
-EMAIL_HOST='smtp.163.com'
-EMAIL_PORT = 25
+# EMAIL_HOST='smtp.163.com'
+# EMAIL_PORT = 25
+
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_PORT = 587
 
 # it would be better to change to your own email account
-EMAIL_HOST_USER = '18599995124@163.com'
-# this is not real password, it needs you to open your email POP3/SMTP service and get the auth code
-EMAIL_HOST_PASSWORD = 'wh170426'
-EMAIL_USE_TLS = False
-EMAIL_FROM = '18599995124@163.com'
+# EMAIL_HOST_USER = '18599995124@163.com'
+# # this is not real password, it needs you to open your email POP3/SMTP service and get the auth code
+# EMAIL_HOST_PASSWORD = 'wh170426'
+# EMAIL_USE_TLS = False
+# EMAIL_FROM = '18599995124@163.com'
+
+EMAIL_HOST_USER = 'zxhme123@gmail.com'
+EMAIL_HOST_PASSWORD = 'zxh2558228'
+EMAIL_USE_TLS = True
+EMAIL_FROM = 'zxhme123@gmail.com'
